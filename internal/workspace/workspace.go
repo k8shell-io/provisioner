6b22a66deeb146409b52a7117c11027b7dc091d5
bruckins <karelnebojsa@seznam.cz>
2025-08-03 23:44:10 +0200

    refactor: move private to config

--- internal/workspace/workspace.go
+++ internal/workspace/workspace.go
@@ -2,16 +2,7 @@ package workspace
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/base64"
-	"encoding/hex"
-	"encoding/pem"
 	"fmt"
-	"math/big"
-	"time"
 
 	identity "github.com/k8shell-io/identity/pkg/models"
 	"github.com/k8shell-io/provisioner/internal/helm"
@@ -100,80 +91,6 @@ func (w *Workspace) Template(ctx context.Context) (string, error) {
 		return "", err
 	}
 	return out, nil
-}
-
-// GenerateSelfSignedCert generates a self-signed TLS certificate and private key
-func (w *Workspace) generateSelfSignedCert(hostname string) (keyPEM, certPEM string, err error) {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to generate RSA private key: %w", err)
-	}
-
-	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
-	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: privateKeyBytes,
-	})
-
-	template := x509.Certificate{
-		SerialNumber: big.NewInt(0).SetBytes(make([]byte, 20)), // Random serial number
-		Subject: pkix.Name{
-			Country:      []string{w.blueprint.K8shelld.Cert.Country},
-			Province:     []string{w.blueprint.K8shelld.Cert.State},
-			Locality:     []string{w.blueprint.K8shelld.Cert.Locality},
-			Organization: []string{w.blueprint.K8shelld.Cert.Organization},
-			CommonName:   w.blueprint.K8shelld.Cert.CommonName,
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(365 * 24 * time.Hour),
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
-	}
-
-	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
-	if err != nil {
-		return "", "", fmt.Errorf("failed to generate serial number: %w", err)
-	}
-	template.SerialNumber = serialNumber
-
-	if hostname != "" && w.namespace != "" {
-		template.DNSNames = []string{fmt.Sprintf("%s.%s", hostname, w.namespace)}
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
-	if err != nil {
-		return "", "", fmt.Errorf("failed to create certificate: %w", err)
-	}
-
-	certPEM = string(pem.EncodeToMemory(&pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certBytes,
-	}))
-
-	keyPEM = string(privateKeyPEM)
-
-	return keyPEM, certPEM, nil
-}
-
-// GenerateAccessKeys generates random access keys similar to Python's secrets.token_hex
-func (w *Workspace) generateAccessKeys() (a1keyB64, a2keyB64 string, err error) {
-	a1Bytes := make([]byte, 16)
-	if _, err := rand.Read(a1Bytes); err != nil {
-		return "", "", fmt.Errorf("failed to generate a1key: %w", err)
-	}
-
-	a2Bytes := make([]byte, 16)
-	if _, err := rand.Read(a2Bytes); err != nil {
-		return "", "", fmt.Errorf("failed to generate a2key: %w", err)
-	}
-
-	a1key := hex.EncodeToString(a1Bytes)
-	a2key := hex.EncodeToString(a2Bytes)
-	a1keyB64 = base64.StdEncoding.EncodeToString([]byte(a1key))
-	a2keyB64 = base64.StdEncoding.EncodeToString([]byte(a2key))
-
-	return a1keyB64, a2keyB64, nil
 }
 
 // ToMap converts any struct to a map[string]interface{} representation
